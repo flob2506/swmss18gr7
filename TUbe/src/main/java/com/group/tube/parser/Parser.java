9477f237d5484d0aedaf3d95483a98d6481a15f6
ccy <ccying95@yahoo.com>
2018-03-21 11:55:21 +0100

    some initial setup

---
+++ TUbe/src/main/java/com/group/tube/parser/Parser.java
@@ -0,0 +1,12 @@
+package com.group.tube.parser;
+import com.group.tube.Models.Episodes;
+
+/**
+ * Created by cychu on 21/03/2018.
+ */
+
+public class Parser {
+    public void parseJSON (String json, Episodes episode) {
+
+    }
+}
