1fbcca89ea9441c1615ea2f509871ab65b56e4db
Daniel <daniel.marogy@gmx.at>
2018-03-21 11:56:26 +0100

    First Video player example

--- TUbe/src/main/java/com/group/tube/MainActivity.java
+++ TUbe/src/main/java/com/group/tube/MainActivity.java
@@ -1,6 +1,9 @@
 package com.group.tube;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);
+
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+
+        Uri uri = Uri.parse("https://tube.tugraz.at/static/mh_default_org/engage-player/8556615e-a07f-43cc-b119-a54bf52cc63e/84a4b941-fb3d-4df9-a6fd-0c619252496b/track.mp4");
+
+
+        videoView.setMediaController(mediaController);
+
+        videoView.setVideoURI(uri);
+        //videoView.requestFocus();
+
+videoView.start();;
+
+
+
     }
 }
