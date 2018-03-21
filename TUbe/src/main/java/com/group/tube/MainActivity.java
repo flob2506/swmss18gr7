0aa0b26ed39895ad80a0656f943b5820a9780cee
Felix Clemens Alexander Auer <felix.auer@student.tugraz.at>
2018-03-21 10:44:31 +0100

    add NetworkConnector Class

--- TUbe/src/main/java/com/group/tube/MainActivity.java
+++ TUbe/src/main/java/com/group/tube/MainActivity.java
@@ -1,6 +1,9 @@
 package com.group.tube;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.group.tube.networking.NetworkConnector;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NetworkConnector networkConnector = new NetworkConnector();
+    }
+
+    private class GetUrlContentTask extends AsyncTask<String, Integer, String> {
+        protected NetworkConnector getNetworkConnector() {
+            NetworkConnector newNetworkConnector = new NetworkConnector();
+            return newNetworkConnector;
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
     }
 }
--- TUbe/src/main/java/com/group/tube/networking/NetworkConnector.java
+++ TUbe/src/main/java/com/group/tube/networking/NetworkConnector.java
@@ -1,6 +1,35 @@
 package com.group.tube.networking;
+import android.os.AsyncTask;
+
+import java.io.IOException;
+
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
-public class NetworkConnector {
+public class NetworkConnector extends AsyncTask{
+    public NetworkConnector(){
+        this.httpClient = new OkHttpClient();
+        try {
+            System.out.println("NetworkConnector "+ this.run("https://tube.tugraz.at/search/episode.json?sid=91ff68f1-7a0d-4655-8cec-643c3cb8b0ae"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+    }
+
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+
+    private OkHttpClient httpClient;
+
+    private String run(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Response response = this.httpClient.newCall(request).execute();
+        return response.body().string();
+    }
 }
