7bf077ebce4b6c3aeccbd28c9772056df2d250dc
Daniel <daniel.marogy@gmx.at>
2018-04-08 10:17:36 +0200

    FullScreen/Centered Video; MediaControls

--- TUbe/src/main/java/com/group/tube/MainActivity.java
+++ TUbe/src/main/java/com/group/tube/MainActivity.java
@@ -8,25 +8,27 @@ import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    VideoView videoView;
+    MediaController mediaController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        VideoView videoView = (VideoView) findViewById(R.id.videoView);
 
-        MediaController mediaController = new MediaController(this);
+        videoView = (VideoView) findViewById(R.id.videoView);
+
+        mediaController = new MediaController(this);
         mediaController.setAnchorView(videoView);
 
         Uri uri = Uri.parse("https://tube.tugraz.at/static/mh_default_org/engage-player/8556615e-a07f-43cc-b119-a54bf52cc63e/84a4b941-fb3d-4df9-a6fd-0c619252496b/track.mp4");
 
 
         videoView.setMediaController(mediaController);
-
         videoView.setVideoURI(uri);
-        //videoView.requestFocus();
-
-videoView.start();;
+        videoView.start();
 
 
 
