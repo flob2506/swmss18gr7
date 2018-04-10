c41610d43dc5a71794d4037c0d1268ff35f7e4c0
Felix Clemens Alexander Auer <felix.auer@student.tugraz.at>
2018-04-10 12:18:07 +0200

    [PG,FA] add testcase to AsyncTask for throwing exceptions

--- TUbe/src/main/java/com/group/tube/networking/NetworkTask.java
+++ TUbe/src/main/java/com/group/tube/networking/NetworkTask.java
@@ -3,6 +3,7 @@ package com.group.tube.networking;
 import android.os.AsyncTask;
 
 import java.io.IOException;
+import java.net.UnknownHostException;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -16,20 +17,21 @@ public class NetworkTask extends AsyncTask {
         this.httpClient = new OkHttpClient();
     }
 
-    private String run(String url) throws IOException {
+    private String run(String url) throws IOException{
         Request request = new Request.Builder()
                 .url(url)
                 .build();
         Response response = this.httpClient.newCall(request).execute();
-        // TODO handle NULL
+
         return response.body().string();
     }
 
     @Override
-    protected Object doInBackground(Object[] objects) {
+    protected Object doInBackground(Object[] objects){
         try {
             return this.run((String)objects[0]);
         } catch (IOException e) {
+            //TODO: handle exceptions in controlled manner
             e.printStackTrace();
             return null;
         }
