d9cda28c18cc16f9e44c43bb392c2ed8922f58ce
Felix Clemens Alexander Auer <felix.auer@student.tugraz.at>
2018-04-17 10:44:59 +0200

    [FL,FA] Rework Network Connection Test, mock JSON and Log

--- TUbe/src/androidTest/java/com/group/tube/VideoPlayerUITest.java
+++ TUbe/src/androidTest/java/com/group/tube/VideoPlayerUITest.java
@@ -1,5 +1,7 @@
 package com.group.tube;
 
+import android.provider.MediaStore;
+import android.support.test.espresso.Espresso;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -9,7 +11,10 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static java.lang.Thread.sleep;
+import static junit.framework.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
@@ -24,7 +29,14 @@ public class VideoPlayerUITest {
     @Test
     public void videoStartsAfterTimeOut() throws InterruptedException {
         sleep(8000);
-        //onView(withId(R.id.videoView)).perform(click());
         assertTrue(((VideoView)mActivityRule.getActivity().findViewById(R.id.videoView)).isPlaying());
     }
+
+    @Test
+    public void videoPause() throws InterruptedException {
+        sleep(8000);
+        ((VideoView)mActivityRule.getActivity().findViewById(R.id.videoView)).pause();
+        assertFalse(((VideoView)mActivityRule.getActivity().findViewById(R.id.videoView)).isPlaying());
+    }
+
 }
