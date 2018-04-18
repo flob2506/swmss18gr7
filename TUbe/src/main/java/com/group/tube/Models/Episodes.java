83a13bfedb3b7cf29aa861bc4843605e463e0488
ProfessorChaos <maximilian.weber@hotmail.com>
2018-04-18 09:40:00 +0200

    [MW, DW] Changed date format

--- TUbe/src/main/java/com/group/tube/Models/Episodes.java
+++ TUbe/src/main/java/com/group/tube/Models/Episodes.java
@@ -1,12 +1,14 @@
 package com.group.tube.Models;
 
+import java.util.Date;
+
 public class Episodes {
     public String id;
     public String course_id;
     public String episode_title;
     public String presenter_url;
     public String presentation_url;
-    public String episode_date;
+    public Date episode_date;
 
     public void setPresenterUrl (String url) {
         presenter_url = url;
