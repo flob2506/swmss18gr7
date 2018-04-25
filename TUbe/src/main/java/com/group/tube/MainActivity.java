afac61fe7266253f165b7e3d5afe415f8039d2cc
ProfessorChaos <maximilian.weber@hotmail.com>
2018-04-25 11:12:05 +0200

    [MW, DM] BackButton Improvements

--- TUbe/src/main/java/com/group/tube/MainActivity.java
+++ TUbe/src/main/java/com/group/tube/MainActivity.java
@@ -2,10 +2,12 @@ package com.group.tube;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 import com.group.tube.Models.Episodes;
 
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
@@ -58,13 +60,43 @@ public class MainActivity extends AppCompatActivity {
                 videoView.start();
 
 
-                Button btn = (Button)findViewById(R.id.BackButton);
+                final Button btn = (Button)findViewById(R.id.BackButton);
+
+                btn.setVisibility(View.INVISIBLE);
+                mediaController.hide();
 
                 btn.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                        // startActivity(new Intent(MainActivity.this, EpisodesOverviewActivity.class));
-                        startActivity(new Intent(MainActivity.this, EpisodesOverviewActivity.class));
+                        ma.finish();
+                        //startActivity(new Intent(MainActivity.this, EpisodesOverviewActivity.class));
+                    }
+                });
+
+
+
+                videoView.setOnTouchListener(new View.OnTouchListener() {
+                    boolean bVideoIsBeingTouched = false;
+                    Handler mHandler = new Handler();
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        if (!bVideoIsBeingTouched) {
+                            bVideoIsBeingTouched = true;
+                            if (mediaController.isShowing()) {
+                                btn.setVisibility(View.INVISIBLE);
+                                mediaController.hide();
+                            } else {
+                                btn.setVisibility(View.VISIBLE);
+                                mediaController.show(0);
+                            }
+                            mHandler.postDelayed(new Runnable() {
+                                public void run() {
+                                    bVideoIsBeingTouched = false;
+                                }
+                            }, 100);
+                        }
+                        return true;
                     }
                 });
 
